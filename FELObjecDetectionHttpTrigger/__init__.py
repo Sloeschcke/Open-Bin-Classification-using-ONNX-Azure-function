54efd7daa3a7ec1ba1018d256be237aad702a95a
Sebastian Loeschcke <Sebastian.Loeschcke@amcsgroup.com>
2023-03-13 11:22:00 +0100

    new output format that is equivalent to old azure output format

--- FELObjecDetectionHttpTrigger/__init__.py
+++ FELObjecDetectionHttpTrigger/__init__.py
@@ -9,7 +9,7 @@ import onnxruntime as ort
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
+    dim_onnx = (800,600) # (width, height) of the model input
     img_base64 = req.params.get('img')
     if not img_base64:
         try:
@@ -20,11 +20,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             img_base64 = req_body.get('img')
 
     if img_base64:
-        img = decode_base64(img_base64)
+        img = decode_base64(img_base64, dim_onnx)
         model_path = 'FELObjecDetectionHttpTrigger\model_keen_frog_ysrb24zd.onnx'
         outputs = run_model(model_path, img)
 
-        return func.HttpResponse(f"The output: {map_outputs(outputs)}")
+        return func.HttpResponse(f"The output: {map_outputs(outputs,dim_onnx)}")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a img_base64 in the query string or in the request body for a personalized response.",
@@ -41,16 +41,15 @@ def preprocess(img_data):
     return norm_img_data
 
 # decode base64 image
-def decode_base64(data):
+def decode_base64(data, dim_onnx):
     img = base64.b64decode(data)
     img = np.fromstring(img, np.uint8)
     img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-    im_target_size = (800,600)
-    img = cv2.resize(img, im_target_size)
+    img = cv2.resize(img, dim_onnx)
     img = img.transpose((2,0,1))
-    img = img.reshape(1, 3, im_target_size[0], im_target_size[1])
+    img = img.reshape(1, 3, dim_onnx[0], dim_onnx[1])
     img = preprocess(img)
-    img = img.reshape(1, 3, im_target_size[1], im_target_size[0])
+    img = img.reshape(1, 3, dim_onnx[1], dim_onnx[0])
     return img
 
 # run model on image
@@ -68,18 +67,43 @@ def load_labels(path):
             labels.append(line.strip())
     return labels
 
+
+def get_box_dims(image_shape, box):
+    box_keys = ['topX', 'topY', 'bottomX', 'bottomY']
+    width, height = image_shape[0], image_shape[1]
+
+    box_dims = dict(zip(box_keys, [coordinate.item() for coordinate in box]))
+
+    box_dims['topX'] = box_dims['topX'] * 1.0 / width
+    box_dims['bottomX'] = box_dims['bottomX'] * 1.0 / width
+    box_dims['topY'] = box_dims['topY'] * 1.0 / height
+    box_dims['bottomY'] = box_dims['bottomY'] * 1.0 / height
+
+    return box_dims
+
 # map mobilenet outputs to classes
-def map_outputs(outputs):
-    """" output is [array([[ 59.14969, 148.71349, 796.6909 , 600.     ]], dtype=float32), array([2], dtype=int64), array([0.39545572], dtype=float32)]
-        output has format : [boxes: array of lists with float32, classes: array of lists with int64, scores: array with float32]
+def map_outputs(outputs, dim_onnx):
     """
+    outputs: list of 3 arrays: boxes, classes, scores
+    dim_onnx: tuple of (width, height) of the model input
+    return output; dict with keys "filename", "boxes"
+        - filename: string
+        - boxes: list of dicts with keys "box", "label", "score"
+    """
+    labels = load_labels('./FELObjecDetectionHttpTrigger/FEL_classes.txt')
+    # map classes to label strings
     boxes = outputs[0]
     classes = outputs[1]
     scores = outputs[2]
-
-    labels = load_labels('./FELObjecDetectionHttpTrigger/FEL_classes.txt')
-    # map classes to label strings
     pred_labels = [labels[i] for i in classes]
+  
+    output = {"filename": "test.jpg", "boxes": []}
+    for i in range(len(boxes)):
+        box_raw = boxes[i]
+        box = get_box_dims(dim_onnx, box_raw)
+        #box = {"topX": box["topX"], "topY": box["topY"], "bottomX": box["bottomX"], "bottomY": box["bottomY"]}
 
-    return [{"boxes": boxes, "labels": pred_labels, "scores": scores}]
-    
+        output["boxes"].append({"box": box, "label": pred_labels[i], "score": scores[i]})
+
+    return output
+
