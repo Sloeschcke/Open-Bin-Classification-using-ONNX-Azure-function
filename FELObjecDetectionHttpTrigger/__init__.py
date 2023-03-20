2f3c810cc9ca8f485ac141568429332014670850
Sebastian Loeschcke <Sebastian.Loeschcke@amcsgroup.com>
2023-03-20 11:18:47 +0100

    yolo compatible

--- FELObjecDetectionHttpTrigger/__init__.py
+++ FELObjecDetectionHttpTrigger/__init__.py
@@ -8,19 +8,26 @@ import os
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    dim_onnx = (800,600) # (width, height) of the model input
+    # dim_onnx = (800,600) # (width, height) of the model input
+    dim_onnx = (640,640) # (width, height) of the model input
     base_path = "FELObjecDetectionHttpTrigger"
     labels = load_labels(os.path.join(base_path, 'FEL_classes.txt'))
-    model_path = os.path.join(base_path, 'model_keen_frog_ysrb24zd.onnx')
+    # model_path = os.path.join(base_path, 'model_keen_frog_ysrb24zd.onnx')
+    use_yolo = False
+    if use_yolo:
+        dim_onnx = (640,640) # (width, height) of the model input
+        model_path = os.path.join(base_path, 'yolov5_model_strong_street_sv6ydlxm.onnx')
     
     # check content type is "image/jpeg" or "image/png"
     content_type = req.headers.get('content-type')
     if content_type == 'image/jpeg' or content_type == 'image/png':
         img_data = req.get_body()
-
     if img_data:    
-        img = decode_byte_arr(img_data, dim_onnx)
+        img, org_image_width, org_image_height = decode_byte_arr(img_data, dim_onnx, do_preprocess=not use_yolo)
         outputs = run_model(model_path, img)
+        if use_yolo:
+            outputs = unwrap_detection_yolo(outputs[0][0], org_image_width, org_image_height)
+            return func.HttpResponse(f"The output: {outputs}")
         return func.HttpResponse(f"The output: {map_outputs(outputs,dim_onnx,labels)}")
     else:
         return func.HttpResponse(
@@ -37,16 +44,28 @@ def preprocess(img_data):
         norm_img_data[i,:,:] = (img_data[i,:,:]/255 - mean_vec[i]) / stddev_vec[i]
     return norm_img_data
 
+def zero_one_rescale(img_data):
+    img_data = np.ascontiguousarray(img_data, dtype=np.float32)  # uint8 to float32 
+    min_val = np.min(img_data)
+    max_val = np.max(img_data)
+    img_data = (img_data - min_val)/(max_val - min_val)
+    return img_data
+
 # decode image
-def decode_byte_arr(img_byte_arr, dim_onnx):
+def decode_byte_arr(img_byte_arr, dim_onnx, do_preprocess=True):
     img = np.frombuffer(img_byte_arr, dtype=np.uint8)
     img = cv2.imdecode(img, cv2.IMREAD_COLOR) # BGR
+    # input size
+    org_image_width, org_image_height = img.shape[1], img.shape[0]
     img = cv2.resize(img, dim_onnx) # HWC
     img = img.transpose((2,0,1)) # HWC to CHW
     img = img.reshape(1, 3, dim_onnx[0], dim_onnx[1]) # add batch dimension
-    img = preprocess(img)
+    if do_preprocess:
+        img = preprocess(img)
+    else:
+        img = zero_one_rescale(img)
     img = img.reshape(1, 3, dim_onnx[1], dim_onnx[0]) # B C H W
-    return img
+    return img, org_image_width, org_image_height
 
 # run model on image
 def run_model(model_path, img):
@@ -77,6 +96,9 @@ def get_box_dims(image_shape, box):
 
     return box_dims
 
+
+
+
 # map mobilenet outputs to classes
 def map_outputs(outputs, dim_onnx, labels):
     """
@@ -100,3 +122,37 @@ def map_outputs(outputs, dim_onnx, labels):
 
     return output
 
+
+def unwrap_detection_yolo(output_data, image_width, image_height ):
+    class_ids = []
+    confidences = []
+    boxes = []
+
+    rows = output_data.shape[0]
+
+    x_factor = image_width / 640
+    y_factor =  image_height / 640
+
+    for r in range(rows):
+        row = output_data[r]
+        confidence = row[4]
+        if confidence >= 0.2:
+
+            classes_scores = row[5:]
+            _, _, _, max_indx = cv2.minMaxLoc(classes_scores)
+            class_id = max_indx[1]
+            if (classes_scores[class_id] > .25):
+
+                confidences.append(confidence)
+
+                class_ids.append(class_id)
+
+                x, y, w, h = row[0].item(), row[1].item(), row[2].item(), row[3].item() 
+                left = int((x - 0.5 * w) * x_factor)
+                top = int((y - 0.5 * h) * y_factor)
+                width = int(w * x_factor)
+                height = int(h * y_factor)
+                box = np.array([left, top, width, height])
+                boxes.append(box)
+
+    return class_ids, confidences, boxes
