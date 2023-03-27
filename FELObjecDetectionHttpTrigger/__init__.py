8d67f87fc5ab3ed25588eb988db781cd12d667f1
Sebastian Loeschcke <Sebastian.Loeschcke@amcsgroup.com>
2023-03-27 12:15:32 +0200

    Revert "working on azure"
    This reverts commit a8171d38f0c5b4b4eef7afa891462e1a19829faf.

--- FELObjecDetectionHttpTrigger/__init__.py
+++ FELObjecDetectionHttpTrigger/__init__.py
@@ -6,6 +6,7 @@ import onnxruntime as ort
 import os 
 
 
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     # dim_onnx = (800,600) # (width, height) of the model input
